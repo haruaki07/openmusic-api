7e577380287c11ff3edf41de391d7f0a3953f481
Haruaki <misbahulk2236@gmail.com>
2023-07-15 01:42:00 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+const Hapi = require("@hapi/hapi");
+require("dotenv/config");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || "0.0.0.0",
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Hello World!";
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
