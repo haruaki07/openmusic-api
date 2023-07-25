d2cb44e3aecdc559cf172225d127ae922858d1f7
Haruaki <misbahulk2236@gmail.com>
2023-07-25 14:00:55 +0000

    feat: export playlist worker

--- src/config.js
+++ src/config.js
@@ -18,6 +18,12 @@ const config = {
   },
   rabbitmq: {
     url: process.env.RABBITMQ_SERVER
+  },
+  smtp: {
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    user: process.env.MAIL_ADDRESS,
+    password: process.env.MAIL_PASSWORD
   }
 };
 
